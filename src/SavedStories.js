46fdc83be26aec131093ff8233bbe97f6ef101b3
bipul8282 <119596693+bipul8282@users.noreply.github.com>
2023-09-30 22:21:54 +0530

    css

--- src/SavedStories.js
+++ src/SavedStories.js
@@ -3,7 +3,7 @@ import React from "react";
 function SavedStories({ stories }) {
   return (
     <div className="saved-stories"> 
-      <h2>Saved Stories</h2>
+      
       {stories.map((story, index) => (
         <div key={index}>
           <p>{story.quote}</p> 
